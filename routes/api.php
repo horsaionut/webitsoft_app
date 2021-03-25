63a1f5ec59d4c7d68a3f33d1fb2f9829b786d82f
user <horsaionut@gmail.com>
2021-03-25 16:58:48 +0200

    Restore good version after tests

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/item',[ItemController::class,'index']);
+Route::get('/items',[ItemController::class,'index']);
 Route::prefix('/item')->group(function(){
     Route::post('/store',[ItemController::class,'store']);
     Route::put('/{id}',[ItemController::class,'update']);
